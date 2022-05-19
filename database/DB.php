f49085e3741b1d819350ada43ee181fd1f00ada6
francois-niang1 <francois.niang@laplateforme.io>
2022-05-19 10:49:35 +0200

    plesk

--- database/DB.php
+++ database/DB.php
@@ -3,7 +3,7 @@
 class Database{
     public static function connect_db(): PDO{
         try {
-            $db = new PDO("mysql:host=localhost;dbname=connect4;charset=utf8", "root", "root");
+            $db = new PDO("mysql:host=localhost;dbname=francois-niang_connect4;charset=utf8", "francois-niangco", "mdpconnect4");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if (!$db) {
                 die("Connexion a la base de donnée impossible");
